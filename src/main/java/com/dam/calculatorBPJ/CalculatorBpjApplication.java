16fc725e5f5881e524e77d17b08991249259e5c0
Juan Diego <juandbp@educastur.org>
2026-01-26 20:29:55 +0100

    Añadido esqueleto Spring Boot

---
+++ src/main/java/com/dam/calculatorBPJ/CalculatorBpjApplication.java
@@ -0,0 +1,13 @@
+package com.dam.calculatorBPJ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorBpjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorBpjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dam/calculatorBPJ/CalculatorBpjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dam.calculatorBPJ;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorBpjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
