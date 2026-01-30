05c65de487c8e685d7e94fe06f32f681259f0bfa
Juan Diego <juandbp@educastur.org>
2026-01-30 12:23:47 +0100

    Se añaden suma, controlador y test unitario.

--- src/test/java/com/dam/calculatorBPJ/CalculatorBpjApplicationTests.java
+++ src/test/java/com/dam/calculatorBPJ/CalculatorBpjApplicationTests.java
@@ -1,13 +1,24 @@
 package com.dam.calculatorBPJ;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test; 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class CalculatorBpjApplicationTests {
-
+	private Calculator calculator = new Calculator();
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	public void testSum(){
+		assertEquals(5, calculator.sum(3,2));
+	}
+
+	@Test
+	public void testSumIncorrecto() {
+    	assertEquals(7, calculator.sum(2, 3));
 	}
 
 }
